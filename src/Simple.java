5b96fdc6c3870ee67c9d0e7c53485ce0f02ee9d0
PratikB96 <pratikbhoite96@gmail.com>
2022-03-03 19:56:08 +0530

    Changes

--- src/Simple.java
+++ src/Simple.java
@@ -4,6 +4,7 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Project to add GitHub.");
+		System.out.println("Main Function Updated.");
 	}
 
 }
